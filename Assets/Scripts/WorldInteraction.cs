6c02226c98c4590e0af16ac781ef6892ea123db7
NguyenThuan9077777 <130331503+NguyenThuan9077777@users.noreply.github.com>
2024-07-19 17:42:02 +0700

    cm

---
+++ Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldInteraction : MonoBehaviour
+{
+
+   
+       public NavMeshAgent playerAgent;
+    private void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())                 
+        {
+            GetInteraction();
+        }
+    }
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(interactionRay,
+            out interactionInfo))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if(interactedObject.tag == "Interactable Object")
+            {
+                Debug.Log("Interactable interacted.");
+            }
+            else
+            {
+                playerAgent.SetDestination(interactionInfo.point);
+            }    
+        }
+    }    
+}
